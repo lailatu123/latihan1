3a689378ff300ae30da94cb429c1f0cc95393a37
lailatu123 <lailahasna177@gmail.com>
2024-05-08 11:14:43 +0700

    Add files via upload

---
+++ ukk_kalkulator/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="vieport" content="width=device-width, initial-scale=1.0">
+	<title>Kalkulator Online</title>
+	<link href = "style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+	<form action="hasil.php">
+		<h1>Kalkulator Online</h1>
+		<input type="number" name="angka1">
+		<select name="operator">
+			<option>+</option>
+			<option>-</option>
+			<option>:</option>
+			<option>x</option>
+		</select>
+		<input type="number" name="angka2">
+		<button type="submit">Hitung</button>
+	</form>
+</body>
+</html>
